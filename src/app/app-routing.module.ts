93269472929af8cafeb32d489595a5270fb13f02
VirtualProgramer <setnight7@gmail.com>
2019-09-04 22:17:44 +0800

    ADD 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// 匯入路由用 compnonent
+import { CharacterListComponent } from './character-list/character-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/characterlist', pathMatch: 'full' },
+  { path: 'characterlist', component: CharacterListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
